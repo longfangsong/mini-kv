3949e17163df4aaf420ed34469673f65ee5c0980
longfangsong <longfangsong@icloud.com>
2020-04-27 08:58:10 +0800

    Basically usable server

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,14 +1,37 @@
 use grpcio::{ChannelBuilder, EnvBuilder};
 use std::sync::Arc;
 use rpc::minikv_grpc::MiniKvServerClient;
-use rpc::minikv::{GetRequest, PutRequest, DeleteRequest, GetResponse, PutResponse, DeleteResponse, ScanResponse};
+use rpc::minikv::{GetRequest, PutRequest, DeleteRequest, GetResponse, PutResponse, DeleteResponse, ScanResponse, ScanRequest};
 
 fn main() {
     let env = Arc::new(EnvBuilder::new().build());
     let ch = ChannelBuilder::new(env).connect("localhost:50051");
     let client = MiniKvServerClient::new(ch);
+    let mut request = PutRequest::default();
+    request.set_key(vec![0x01u8, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]);
+    request.set_value(vec![0x01u8].repeat(256));
+    let response = client.put(&request).unwrap();
+    println!("{:?}", response);
     let mut request = GetRequest::default();
-    request.set_key(vec![0x01]);
+    request.set_key(vec![0x01u8, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]);
     let response = client.get(&request).unwrap();
+    println!("{:?}", response);
+    let mut request = PutRequest::default();
+    request.set_key(vec![0x01u8, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]);
+    request.set_value(vec![0x02u8].repeat(256));
+    let response = client.put(&request).unwrap();
+    println!("{:?}", response);
+    let mut request = GetRequest::default();
+    request.set_key(vec![0x01u8, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]);
+    let response = client.get(&request).unwrap();
+    println!("{:?}", response);
+    let mut request = PutRequest::default();
+    request.set_key(vec![0x01u8, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x09]);
+    request.set_value(vec![0x03u8].repeat(256));
+    let response = client.put(&request).unwrap();
+    println!("{:?}", response);
+    let mut request = ScanRequest::default();
+    request.set_cursor(vec![0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0]);
+    let response = client.scan(&request).unwrap();
     println!("{:?}", response);
 }
--- src/server/main.rs
+++ src/server/main.rs
@@ -3,62 +3,123 @@ use std::io::Read;
 use std::sync::Arc;
 use std::{io, thread};
 
-use futures::channel::oneshot;
-use futures::executor::block_on;
-use futures::compat::{Future01CompatExt, Compat};
+use futures::{
+    channel::oneshot,
+    executor::block_on,
+    compat::Future01CompatExt,
+};
 use futures01::future::Future;
 use grpcio::{ChannelBuilder, Environment, ResourceQuota, RpcContext, ServerBuilder, UnarySink};
+use futures_locks::Mutex;
+use std::collections::HashMap;
 
 #[derive(Clone)]
-struct MockKVServer;
+struct KVServer {
+    // todo: lock lines instead of whole table
+    store: Arc<Mutex<HashMap<[u8; 8], [u8; 256]>>>
+}
 
-impl rpc::minikv_grpc::MiniKvServer for MockKVServer {
+impl rpc::minikv_grpc::MiniKvServer for KVServer {
     fn get(&mut self, ctx: RpcContext<'_>, req: GetRequest, sink: UnarySink<GetResponse>) {
         let mut response = GetResponse::default();
-        let result = vec![0x01u8].iter().cycle().take(256).copied().collect();
-        response.set_value(result);
-        let f = sink
-            .success(response)
-            .map_err(move |e| println!("failed to reply {:?}: {:?}", req, e))
-            .map(|_| ());
+        let mut key = [0u8; 8];
+        for (i, v) in req.key.iter().enumerate() {
+            if let Some(r) = key.get_mut(i) {
+                *r = *v;
+            } else {
+                break;
+            }
+        }
+        let f = self.store.lock()
+            .then(move |it| {
+                let guard = it.unwrap();
+                let value = guard.get(&key).unwrap();
+                response.set_value(value.to_vec());
+                sink.success(response)
+                    .map_err(move |e| println!("failed to reply {:?}: {:?}", req, e))
+                    .map(|_| ())
+            });
         ctx.spawn(f)
     }
 
     fn put(&mut self, ctx: RpcContext<'_>, req: PutRequest, sink: UnarySink<PutResponse>) {
         let mut response = PutResponse::default();
+        let mut key = [0u8; 8];
+        let mut value = [0u8; 256];
         response.set_success(true);
-        let f = sink
-            .success(response)
-            .map_err(move |e| println!("failed to reply {:?}: {:?}", req, e))
-            .map(|_| ());
+        for (i, v) in req.key.iter().enumerate() {
+            if let Some(r) = key.get_mut(i) {
+                *r = *v;
+            } else {
+                response.set_success(false);
+                break;
+            }
+        }
+        for (i, v) in req.value.iter().enumerate() {
+            if let Some(r) = value.get_mut(i) {
+                *r = *v;
+            } else {
+                response.set_success(false);
+                break;
+            }
+        }
+        let f = self.store.lock()
+            .map(move |mut it| {
+                it.insert(key, value);
+            })
+            .then(|_| {
+                response.set_success(true);
+                sink.success(response)
+                    .map_err(move |e| println!("failed to reply {:?}: {:?}", req, e))
+                    .map(|_| ())
+            });
         ctx.spawn(f)
     }
 
     fn delete(&mut self, ctx: RpcContext<'_>, req: DeleteRequest, sink: UnarySink<DeleteResponse>) {
         let mut response = DeleteResponse::default();
-        response.set_success(true);
-        let f = sink
-            .success(response)
-            .map_err(move |e| println!("failed to reply {:?}: {:?}", req, e))
-            .map(|_| ());
+        let mut key = [0u8; 8];
+        for (i, v) in req.key.iter().enumerate() {
+            if let Some(r) = key.get_mut(i) {
+                *r = *v;
+            } else {
+                break;
+            }
+        }
+        let f = self.store.lock()
+            .map(move |mut it| {
+                it.remove(&key)
+            }).then(|_| {
+            response.set_success(true);
+            sink.success(response)
+                .map_err(move |e| println!("failed to reply {:?}: {:?}", req, e))
+                .map(|_| ())
+        });
         ctx.spawn(f)
     }
 
     fn scan(&mut self, ctx: RpcContext<'_>, req: ScanRequest, sink: UnarySink<ScanResponse>) {
         let mut response = ScanResponse::default();
         response.set_cursor(vec![0x0]);
-        response.set_result(vec![vec![0x1], vec![0x2]].into());
-        let f = sink
-            .success(response)
-            .map_err(move |e| println!("failed to reply {:?}: {:?}", req, e))
-            .map(|_| ());
+        let f = self.store.lock()
+            .then(move |it| {
+                let guard = it.unwrap();
+                let value: Vec<Vec<u8>> = guard.keys().map(|it| it.to_vec()).collect();
+                response.set_result(value.into());
+                sink.success(response)
+                    .map_err(move |e| println!("failed to reply {:?}: {:?}", req, e))
+                    .map(|_| ())
+            });
         ctx.spawn(f)
     }
 }
 
 fn main() {
     let env = Arc::new(Environment::new(1));
-    let service = rpc::minikv_grpc::create_mini_kv_server(MockKVServer);
+    let server = KVServer {
+        store: Arc::new(Mutex::new(HashMap::new()))
+    };
+    let service = rpc::minikv_grpc::create_mini_kv_server(server);
     let quota = ResourceQuota::new(Some("MiniKVServerQuota")).resize_memory(1024 * 1024);
     let ch_builder = ChannelBuilder::new(env.clone()).set_resource_quota(quota);
 
